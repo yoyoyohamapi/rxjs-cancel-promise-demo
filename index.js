38dfdb5fc7cd5a34d95af912477c9fda62044275
surejinwu <surejinwu@tencent.com>
2018-04-03 17:42:52 +0800

    feat: demo 深化

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 import { fromPromise } from 'rxjs/observable/fromPromise'
 import { fromEvent } from 'rxjs/observable/fromEvent'
-import { takeUntil, switchMap, tap } from 'rxjs/operators'
-
-const rand = () => Math.floor(Math.random() * 100)
-
-const getData = function() {
-  return new Promise((resolve, reject) => {
-    const data = rand()
-    setTimeout(() => resolve(data), 2000)
-  })
-}
-
+import { merge } from 'rxjs/observable/merge'
+import { range } from 'rxjs/observable/range'
+import { ajax } from 'rxjs/observable/dom/ajax'
+import { takeUntil, switchMap, mergeMap, tap, mapTo, delay } from 'rxjs/operators'
 const $show = document.querySelector('#show')
 
 // 定义开始点击
 const startClick$ = fromEvent(document.querySelector('#start'), 'click')
 // 定义结束点击
 const endClick$ = fromEvent(document.querySelector('#end'), 'click')
+const blur$ = fromEvent(window, 'blur')
 
-// 定义请求流
-const createRequest = () => 
-  fromPromise(getData())
+// 当手动点击结束或者离开窗口，则停止请求
+const endRequest$ = merge(endClick$, blur$)
+
+const createRequest = rand => {
+  return ajax(`test.png?${rand}`)
     .pipe(
-      tap(() => console.log('创建了一个请求')),
-      takeUntil(endClick$), 
+      delay(500),
+      takeUntil(endRequest$),
+      mapTo(rand)
     )
+}
 
 // 定义显示流
-// 单击开始按钮开始，多次点击只响应最近一次（swichMap）
-// 单击结束按钮停止请求
+// 单击开始按钮开始，模拟 1000 个图像请求
 const show$ = startClick$
   .pipe(
-    switchMap(createRequest)
+    mergeMap(() => 
+      range(0, 1000).pipe(
+        mergeMap(i => createRequest(i)),
+      )
+    )
   )
 
-const setShowContent = content => $show.textContent = content
+const setShowContent = content => {
+  $show.textContent = content
+}
 
-startClick$.subscribe(e => setShowContent('正在查询...'))
-show$.subscribe(setShowContent)
-endClick$.subscribe(e => setShowContent('停止查询'))
+startClick$.subscribe(() => setShowContent('开始请求...'))
+show$.subscribe({
+  onComplete() {
+    setShowContent('所有请求完毕')
+  }
+})
+endRequest$.subscribe(() => setShowContent('结束！'))
